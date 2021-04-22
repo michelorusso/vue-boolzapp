a6f1df9dbc54c15435bf1c1e8d928430edcea79b
Michele Lorusso <kirito102095@hotmail.it>
2021-04-22 22:13:40 +0200

    Milestone 2 Complete

--- js/script.js
+++ js/script.js
@@ -11,24 +11,27 @@ var app = new Vue(
     {
     el: '#root',
     data: {  
+		showTextContact: 0,
 		contacts: [
 			{
 			name: 'Clara',
 			avatar: 'img/avatar_io.jpg',
 			visible: true,
+			chatActive: 'courentchat',
+			time: '09:37',
 			messages: [
 				{
-					date: '10/01/2020 15:30:55',
+					date: '15/04/2021 15:30:55',
 					text: 'Hai portato a spasso il cane?',
 					status: 'sent'
 				},
 				{
-					date: '10/01/2020 15:50:00',
+					date: '15/04/2021 15:50:00',
 					text: 'Ricordati di dargli da mangiare',
 					status: 'sent'
 				},
 				{
-					date: '10/01/2020 16:15:22',
+					date: '18/01/2021 16:15:22',
 					text: 'Tutto fatto!',
 					status: 'received'
 				}
@@ -37,29 +40,33 @@ var app = new Vue(
 		{
 			name: 'Paolo',
 			avatar: 'img/avatar_8.jpg',
-			visible: true,
+			visible: false,
+			chatActive: '',
+			time: '19:47',
 			messages: [
 				{
-					date: '20/03/2020 16:30:00',
+					date: '20/04/2021 16:30:00',
 					text: 'Ciao come stai?',
 					status: 'sent'
 				},
 				{
-					date: '20/03/2020 16:30:55',
+					date: '21/04/2021 16:30:55',
 					text: 'Bene grazie! Stasera ci vediamo?',
 					status: 'received'
 				},
 				{
-					date: '20/03/2020 16:35:00',
+					date: '22/04/2021 16:35:00',
 					text: 'Mi piacerebbe ma devo andare a fare la spesa.',
 					status: 'sent'
 				}
 			],
 		},
 		{
-			name: 'Samuele',
+			name: 'Lele',
 			avatar: 'img/avatar_3.jpg',
-			visible: true,
+			visible: false,
+			chatActive: '',
+			time: '15:18',
 			messages: [
 				{
 					date: '28/03/2020 10:10:40',
@@ -81,15 +88,17 @@ var app = new Vue(
 		{
 			name: 'Luisa',
 			avatar: 'img/avatar_6.jpg',
-			visible: true,
+			visible: false,
+			chatActive: '',
+			time: '13:32',
 			messages: [
 				{
-					date: '10/01/2020 15:30:55',
+					date: '19/03/2021 15:30:55',
 					text: 'Lo sai che ha aperto una nuova pizzeria?',
 					status: 'sent'
 				},
 				{
-					date: '10/01/2020 15:50:00',
+					date: '19/03/2021 15:50:00',
 					text: 'Si, ma preferirei andare al cinema',
 					status: 'received'
 				}
@@ -98,7 +107,11 @@ var app = new Vue(
 	]
     }, 
     methods: {
-
+		showChat(index) {
+			this.showTextContact = index;
+			this.contacts.forEach((element, i) => (i != index) ? element.chatActive = "" : element.chatActive = "courentchat");
+		}	
+		
 	}
 
 });
